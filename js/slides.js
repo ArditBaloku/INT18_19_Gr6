53463c6d7afe311e8c229527c2fb081b66e49c8b
arbenamusa <43915605+arbenamusa@users.noreply.github.com>
2018-11-23 20:29:42 +0100

    Create slides.js

---
+++ js/slides.js
@@ -0,0 +1,33 @@
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var circles = document.getElementsByClassName("circles");
+    if (n > slides.length)
+    {
+        slideIndex = 1;
+    }
+    if (n < 1)
+    {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++)
+    {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < circles.length; i++)
+    {
+        circles[i].className = circles[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    circles[slideIndex-1].className += " active";
+  }
+  function plusSlides(n)
+  {
+    showSlides(slideIndex += n);
+  }
+
+  function currentSlide(n)
+  {
+    showSlides(slideIndex=n);
+  }
